4ca8d47dc276d9adec7c7b554e2d1cef70ac7006
Oleg G.Kapranov <oleg@vertiopia.com>
2016-08-15 11:12:28 +0300

    Updated mirage/factories/posts.js

--- mirage/factories/posts.js
+++ mirage/factories/posts.js
@@ -1,4 +1,8 @@
-import { Factory } from 'ember-cli-mirage';
+// import { Factory } from 'ember-cli-mirage';
+import Mirage, {faker}  from 'ember-cli-mirage';
 
-export default Factory.extend({
+// export default Factory.extend({
+export default Mirage.Factory.extend({
+  title: faker.hacker.phrase,
+  text: faker.lorem.sentence
 });
