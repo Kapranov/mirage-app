2862635337a36f907794e58664bda2e54fbacf0e
Oleg G.Kapranov <oleg@vertiopia.com>
2016-08-15 10:54:16 +0300

    Updated app/router.js

--- app/router.js
+++ app/router.js
@@ -7,7 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('posts');
+  // this.route('posts');
+  this.route('posts', {path: '/posts/:post_id'});
 });
 
 export default Router;
